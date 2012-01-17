998394ee510d5944478224ab443a535929895859
Daniel Pérez Rada <dperezrada@gmail.com>
2012-01-17 20:11:56 -0300

    Changing port

--- app.js
+++ app.js
@@ -32,5 +32,7 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/sapea', routes.sapea);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
